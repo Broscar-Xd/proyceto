e0fb223f7419b749ddec7248f3ac5ccab26f0920
Broscar-Xd <diegobr3d@gmail.com>
2020-08-12 12:50:57 -0500

    Primer guardado de proyecto

---
+++ portada.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portada</title>
+</head>
+<body>
+    <h1>Portada</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea odio itaque quas necessitatibus! Perferendis, officia? Odio consectetur placeat aperiam saepe, est libero animi non, quis porro suscipit corporis facere accusamus.</p>
+</body>
+</html>
